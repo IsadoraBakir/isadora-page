f6966715be109154044ecc3abf7af79555032718
Isadora Bakir <isadorabakir@gmail.com>
2024-11-14 19:24:18 -0300

    feat: developing banner component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FeaturesModule } from '../../features/features.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FeaturesModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
