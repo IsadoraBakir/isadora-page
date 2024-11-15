58b49286e8b268e9a21bc05efa2556115725389c
Isadora Bakir <isadorabakir@gmail.com>
2024-11-15 08:40:29 -0300

    Auto-generated commit

--- src/app/features/technologies/technologies.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-technologies',
-  standalone: false,
-  templateUrl: './technologies.component.html',
-  styleUrl: './technologies.component.scss',
-})
-export class TechnologiesComponent {}
